6e3c77798d9e2cd98d15be2ecaa3af1294bf0629
valerialeticia <43298249+valerialeticia@users.noreply.github.com>
2018-09-24 17:55:01 -0300

    Initial commit
    simple CRUD

---
+++ mercadinhoBigBOM/mercadinhoBigBOM/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace mercadinhoBigBOM
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_inserir = new System.Windows.Forms.Button();
+            this.label_cadastro = new System.Windows.Forms.Label();
+            this.label_nome = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label_endereco = new System.Windows.Forms.Label();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.label_cidade = new System.Windows.Forms.Label();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.DG_mercadinho = new System.Windows.Forms.DataGridView();
+            this.btn_cadastrar = new System.Windows.Forms.Button();
+            this.btn_deletar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DG_mercadinho)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_inserir
+            // 
+            this.btn_inserir.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn_inserir.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_inserir.Location = new System.Drawing.Point(615, 49);
+            this.btn_inserir.Name = "btn_inserir";
+            this.btn_inserir.Size = new System.Drawing.Size(112, 38);
+            this.btn_inserir.TabIndex = 0;
+            this.btn_inserir.Text = "Inserir";
+            this.btn_inserir.UseVisualStyleBackColor = false;
+            this.btn_inserir.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label_cadastro
+            // 
+            this.label_cadastro.AutoSize = true;
+            this.label_cadastro.Font = new System.Drawing.Font("Ravie", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_cadastro.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label_cadastro.Location = new System.Drawing.Point(293, 20);
+            this.label_cadastro.Name = "label_cadastro";
+            this.label_cadastro.Size = new System.Drawing.Size(195, 39);
+            this.label_cadastro.TabIndex = 1;
+            this.label_cadastro.Text = "Cadastro";
+            // 
+            // label_nome
+            // 
+            this.label_nome.AutoSize = true;
+            this.label_nome.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_nome.Location = new System.Drawing.Point(112, 138);
+            this.label_nome.Name = "label_nome";
+            this.label_nome.Size = new System.Drawing.Size(36, 15);
+            this.label_nome.TabIndex = 2;
+            this.label_nome.Text = "Nome";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(154, 136);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(193, 20);
+            this.txtNome.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Id";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(37, 136);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(47, 20);
+            this.txtId.TabIndex = 5;
+            // 
+            // label_endereco
+            // 
+            this.label_endereco.AutoSize = true;
+            this.label_endereco.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_endereco.Location = new System.Drawing.Point(12, 173);
+            this.label_endereco.Name = "label_endereco";
+            this.label_endereco.Size = new System.Drawing.Size(55, 15);
+            this.label_endereco.TabIndex = 6;
+            this.label_endereco.Text = "Endereço";
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Location = new System.Drawing.Point(71, 170);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(138, 20);
+            this.txtEndereco.TabIndex = 7;
+            // 
+            // label_cidade
+            // 
+            this.label_cidade.AutoSize = true;
+            this.label_cidade.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_cidade.Location = new System.Drawing.Point(225, 173);
+            this.label_cidade.Name = "label_cidade";
+            this.label_cidade.Size = new System.Drawing.Size(42, 15);
+            this.label_cidade.TabIndex = 8;
+            this.label_cidade.Text = "Cidade";
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.Location = new System.Drawing.Point(273, 170);
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.Size = new System.Drawing.Size(119, 20);
+            this.txtCidade.TabIndex = 9;
+            // 
+            // DG_mercadinho
+            // 
+            this.DG_mercadinho.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DG_mercadinho.Location = new System.Drawing.Point(21, 235);
+            this.DG_mercadinho.Name = "DG_mercadinho";
+            this.DG_mercadinho.Size = new System.Drawing.Size(573, 201);
+            this.DG_mercadinho.TabIndex = 10;
+            // 
+            // btn_cadastrar
+            // 
+            this.btn_cadastrar.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn_cadastrar.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_cadastrar.Location = new System.Drawing.Point(489, 199);
+            this.btn_cadastrar.Name = "btn_cadastrar";
+            this.btn_cadastrar.Size = new System.Drawing.Size(105, 30);
+            this.btn_cadastrar.TabIndex = 11;
+            this.btn_cadastrar.Text = "Mostrar banco";
+            this.btn_cadastrar.UseVisualStyleBackColor = false;
+            this.btn_cadastrar.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // btn_deletar
+            // 
+            this.btn_deletar.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn_deletar.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_deletar.Location = new System.Drawing.Point(615, 103);
+            this.btn_deletar.Name = "btn_deletar";
+            this.btn_deletar.Size = new System.Drawing.Size(112, 38);
+            this.btn_deletar.TabIndex = 12;
+            this.btn_deletar.Text = "Deletar";
+            this.btn_deletar.UseVisualStyleBackColor = false;
+            this.btn_deletar.Click += new System.EventHandler(this.btn_deletar_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(3, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 38);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(615, 160);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 38);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Alterar";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_deletar);
+            this.Controls.Add(this.btn_cadastrar);
+            this.Controls.Add(this.DG_mercadinho);
+            this.Controls.Add(this.txtCidade);
+            this.Controls.Add(this.label_cidade);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.label_endereco);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label_nome);
+            this.Controls.Add(this.label_cadastro);
+            this.Controls.Add(this.btn_inserir);
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DG_mercadinho)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_inserir;
+        private System.Windows.Forms.Label label_cadastro;
+        private System.Windows.Forms.Label label_nome;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label_endereco;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.Label label_cidade;
+        private System.Windows.Forms.TextBox txtCidade;
+        private System.Windows.Forms.DataGridView DG_mercadinho;
+        private System.Windows.Forms.Button btn_cadastrar;
+        private System.Windows.Forms.Button btn_deletar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ mercadinhoBigBOM/mercadinhoBigBOM/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace mercadinhoBigBOM
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        //botao inserir
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //conexao banco de dados
+            try
+            {
+                //passa a string de conexao
+                MySqlConnection objcon = new MySqlConnection("server=localhost;port=3306;User Id=root;database=mercadinho;password=@Solemar157");
+                //abre o banco de dados
+                objcon.Open();
+                //comando mysql para inserir dados na tabela
+                MySqlCommand objCmd = new MySqlCommand("insert into tb_dados(txt_id, txt_nome, txt_endereco, txt_cidade) values(null, ?, ?, ?)", objcon);
+                //parametros de comando
+                objCmd.Parameters.Add("@txt_id", MySqlDbType.VarChar, 60).Value = txtNome.Text;
+                objCmd.Parameters.Add("@txt_endereco", MySqlDbType.VarChar, 50).Value = txtEndereco.Text;
+                objCmd.Parameters.Add("@txt_cidade", MySqlDbType.VarChar, 30).Value = txtCidade.Text;
+                //comando para executar o query
+                objCmd.ExecuteNonQuery();
+
+                MessageBox.Show("Cadastrado com sucesso!!");
+                
+                //fecha o banco de dados
+                objcon.Close();
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Nao conectado:" + erro);
+            }
+        }
+        //botao do datagrid
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //passa a string de conexao novamente
+            MySqlConnection objcon = new MySqlConnection("server=localhost;port=3306;User Id=root;database=mercadinho;password=@Solemar157");
+            //cria um objeto data table 
+            DataTable dt = new DataTable();
+            //a funcao data adapter adapta os dados do bd para o vs
+            MySqlDataAdapter da = new MySqlDataAdapter("select * from tb_dados", objcon);
+            objcon.Open();
+            //adiciona ou atualiza os dados que ficam na memoria ou seja no dataset
+            da.Fill(dt);
+            //preenche o data grid
+            DG_mercadinho.DataSource = dt;
+            objcon.Close();
+
+
+        }
+
+        //botao deletar
+        private void btn_deletar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               
+                MySqlConnection objcon = new MySqlConnection("server=localhost;port=3306;User Id=root;database=mercadinho;password=@Solemar157");
+                objcon.Open();
+                //comando mysql com seus devidos parametros para deletar dados da tabela
+                MySqlCommand objCmd = new MySqlCommand("delete from tb_dados where txt_id  = ?", objcon);
+                //da um clear para evitar problemas
+                objCmd.Parameters.Clear();
+                //adiconar parametros
+                objCmd.Parameters.Add("@txt_id", MySqlDbType.Int32).Value = txtId.Text;
+                
+                //para saber qual tipo de comando ira usar e executa o comando
+                objCmd.CommandType = CommandType.Text;
+                objCmd.ExecuteNonQuery();
+                //fecha a conexao
+                objcon.Close();
+                MessageBox.Show("Registro deletado com sucesso!!");
+                
+
+
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Nao foi possivel deletar:" + erro);
+            }
+        }
+
+        //botao de buscar
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            try
+            {
+                //passa a string de conexao
+                MySqlConnection objcon = new MySqlConnection("server=localhost;port=3306;User Id=root;database=mercadinho;password=@Solemar157");
+                //abre o banco de dados
+                objcon.Open();
+                MySqlCommand objCmd = new MySqlCommand("select txt_nome, txt_endereco, txt_cidade from tb_dados where txt_id = ?", objcon);
+                //da um clear para evitar problemas
+                objCmd.Parameters.Clear();
+                //adiconar parametros
+                objCmd.Parameters.Add("@txt_id", MySqlDbType.Int32).Value = txtId.Text;
+                //para saber qual tipo de comando ira usar e executa o comando
+                objCmd.CommandType = CommandType.Text;
+                //recebe o conteudo que vem do banco
+                MySqlDataReader dr;
+                dr = objCmd.ExecuteReader();
+                //faz a leitura para o que veio para o dr e tambem insere as informacoes recebidas pelo banco
+                dr.Read();
+
+                txtNome.Text = dr.GetString(0);
+                txtEndereco.Text = dr.GetString(1);
+                txtCidade.Text = dr.GetString(2);
+
+
+                //fecha a conexao
+                objcon.Close();
+
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Erro ao buscar o registro!" + erro);
+            }
+        }
+
+        //botao de alterar
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //passa a string de conexao
+                MySqlConnection objcon = new MySqlConnection("server=localhost;port=3306;User Id=root;database=mercadinho;password=@Solemar157");
+                //abre o banco de dados
+                objcon.Open();
+                MySqlCommand objCmd = new MySqlCommand("update tb_dados set txt_nome = ?, txt_endereco = ?, txt_cidade = ? where txt_id = ?", objcon);
+                //da um clear para evitar problemas
+                objCmd.Parameters.Clear();
+                //passando os parametros
+                objCmd.Parameters.Add("@txt_nome", MySqlDbType.VarChar, 60).Value = txtNome.Text;
+                objCmd.Parameters.Add("@txt_endereco", MySqlDbType.VarChar, 50).Value = txtEndereco.Text;
+                objCmd.Parameters.Add("@txt_cidade", MySqlDbType.VarChar, 30).Value = txtCidade.Text;
+                objCmd.Parameters.Add("@txt_id", MySqlDbType.Int32).Value = txtId.Text;
+
+                //executando o comando
+                objCmd.CommandType = CommandType.Text;
+                objCmd.ExecuteNonQuery();
+                
+                //fecha a conexao
+                objcon.Close();
+
+                MessageBox.Show("Alteracao feita com sucesso!!");
+
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Não foi possível alterar o registro" + erro);
+            }
+        }
+    }
+}
